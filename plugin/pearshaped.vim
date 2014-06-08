b0cb29f2b988682875933b484736ede9efd906dc
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-07 21:54:26 -0400

    Source a specified vimrc

--- plugin/pearshaped.vim
+++ plugin/pearshaped.vim
@@ -1,3 +1,8 @@
+
+if ! exists('g:pearshaped_directory')
+  let g:pearshaped_directory = '~/.pearshaped'
+endif
+
 function! s:ClearMappings()
   mapclear
 endfunction
@@ -6,10 +11,17 @@ function! s:ResetOptionsToDefaults()
   set all&
 endfunction
 
+function! s:SourceVimrc(name)
+  let s:file = g:pearshaped_directory . '/' . a:name . "/vimrc"
+
+  execute "source " s:file
+endfunction
+
 " Add a user and source their vimrc
 function! s:PearShaped(name)
   call s:ClearMappings()
   call s:ResetOptionsToDefaults()
+  call s:SourceVimrc(a:name)
 endfunction
 
 command! -nargs=1 PearShaped call s:PearShaped(<f-args>)
--- spec/plugin/pearshaped_spec.rb
+++ spec/plugin/pearshaped_spec.rb
@@ -5,7 +5,7 @@ describe 'pearshaped.vim' do
     it 'clears all mappings' do
       vim.command 'nnoremap x :echo "Hello"'
 
-      vim.command 'PearShaped("ben")'
+      vim.command 'PearShaped "gabe"'
 
       expect(vim).to have_no_mapping_from('x')
     end
@@ -13,9 +13,21 @@ describe 'pearshaped.vim' do
     it 'resets all options to their defaults' do
       vim.command 'set cursorcolumn'
 
-      vim.command 'PearShaped("ben")'
+      vim.command 'PearShaped "gabe"'
 
-      expect(vim.command('echo &cursorcolumn')).to eq '0'
+      expect(set?('cursorcolumn')).to be false
     end
+
+    it 'sources a vimrc if it is available' do
+      vim.command "let g:pearshaped_directory = '#{ROOT}/spec/fixtures'"
+
+      vim.command 'PearShaped gabe'
+
+      expect(set?('cursorcolumn')).to be true
+    end
+  end
+
+  def set?(option)
+    vim.command('echo &cursorcolumn') == '1'
   end
 end
