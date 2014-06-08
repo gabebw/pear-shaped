af703bf314e29a6180f32807948c3120d33874e9
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-07 23:15:26 -0400

    Use a new vim instance per spec

--- spec/plugin/pear_shaped_spec.rb
+++ spec/plugin/pear_shaped_spec.rb
@@ -2,38 +2,54 @@ require 'spec_helper'
 
 describe 'pear_shaped.vim' do
   it 'sets g:pear_shaped_directory to ~/.pear_shaped by default' do
-    default_directory = vim.echo('g:pear_shaped_directory')
+    with_clean_vim do |clean_vim|
+      default_directory = clean_vim.echo('g:pear_shaped_directory')
 
-    expect(default_directory).to eq '~/.pear_shaped'
+      expect(default_directory).to eq '~/.pear_shaped'
+    end
   end
 
   context 'when switching to a new user' do
     it 'clears all mappings' do
-      vim.command 'nnoremap x :echo "Hello"'
+      with_clean_vim do |clean_vim|
+        clean_vim.command 'nnoremap x :echo "Hello"'
 
-      vim.command 'PearShaped gabe'
+        clean_vim.command 'PearShaped gabe'
 
-      expect(vim).to have_no_mapping_from('x')
+        expect(clean_vim).to have_no_mapping_from('x')
+      end
     end
 
     it 'resets all options to their defaults' do
-      vim.set 'cursorcolumn'
+      with_clean_vim do |clean_vim|
+        clean_vim.set 'cursorcolumn'
 
-      vim.command 'PearShaped gabe'
+        clean_vim.command 'PearShaped gabe'
 
-      expect(set?('cursorcolumn')).to be false
+        expect(set?(clean_vim, 'cursorcolumn')).to be false
+      end
     end
 
     it 'sources a vimrc' do
-      vim.command "let g:pear_shaped_directory = '#{ROOT}/spec/fixtures'"
+      with_clean_vim do |clean_vim|
+        clean_vim.command "let g:pear_shaped_directory = '#{ROOT}/spec/fixtures'"
 
-      vim.command 'PearShaped gabe'
+        clean_vim.command 'PearShaped gabe'
 
-      expect(set?('cursorcolumn')).to be true
+        expect(set?(clean_vim, 'cursorcolumn')).to be true
+      end
     end
   end
 
-  def set?(option)
-    vim.echo('&cursorcolumn') == '1'
+  def set?(vim_instance, option)
+    vim_instance.echo('&cursorcolumn') == '1'
+  end
+
+  def with_clean_vim
+    clean_vim = Vimrunner.start
+    clean_vim.add_plugin(File.join(ROOT, 'plugin'), 'pear_shaped.vim')
+    yield(clean_vim)
+  ensure
+    clean_vim.kill
   end
 end
