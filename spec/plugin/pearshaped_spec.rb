4bc4ee6dd45bdc70c7eda099c5256578d4d4ca86
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-07 21:13:08 -0400

    Add RSpec

---
+++ spec/plugin/pearshaped_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe 'pearshaped.vim' do
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require 'vimrunner'
+require 'vimrunner/rspec'
+
+Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
+
+ROOT = File.expand_path('../..', __FILE__)
+
+Vimrunner::RSpec.configure do |config|
+  config.reuse_server = true
+  config.start_vim do
+    vim = Vimrunner.start
+
+    vim.add_plugin(File.join(ROOT, 'plugin'), 'pearshaped.vim')
+
+    vim
+  end
+end
+
+RSpec.configure do |config|
+  config.order = :random
+end
